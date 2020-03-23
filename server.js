21413924c665022ba8069a2323c07c55afa4c838
Josiah Kwesi Paintsil <Baller99@josiahs-mbp.colgate.edu>
2020-03-22 21:59:48 -0400

    adding the server

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var port = process.env.PORT || 8080
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', function(request, response){
+    response.render('home.ejs')
+})
+
+app.listen(port, function(){
+    console.log("IT'S ALIVE!!!! -Frankenstein")
+})
