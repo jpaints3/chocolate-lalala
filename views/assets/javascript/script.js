20ae0a4f2bc4882eb82718f8100c6946f81bafa1
Josiah Kwesi Paintsil <Baller99@josiahs-mbp.colgate.edu>
2020-03-23 20:52:08 -0400

    js

--- views/assets/javascript/script.js
+++ views/assets/javascript/script.js
@@ -1,7 +1,3 @@
-$(window).addEventListener("load", function(){
-    alert("Hi, thanks for visiting my site! Although, this page can run on any browser size, it looks best on a browser size of 700x1000.");
-});
-
 window.onload = function(){
     alert("Hi, thanks for visiting my site! Although, this page can run on any browser size, it looks best on a browser size of 700x1000.");
 };
