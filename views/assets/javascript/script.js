84da9660df0af32c8d03b41a19329087d302fa9b
Josiah Kwesi Paintsil <Baller99@josiahs-mbp.colgate.edu>
2020-03-23 20:11:14 -0400

    animation

--- views/assets/javascript/script.js
+++ views/assets/javascript/script.js
@@ -1,8 +1,8 @@
-$window.addEventListener("load", function(){
+$(window).addEventListener("load", function(){
     $window.alert("Hi, thanks for visiting my site! Although, this page can run on any browser size, it looks best on a browser size of 700x1000.");
 });
 
-$window.addEventListener("load", function startTimer(){
+$(window).addEventListener("load", function startTimer(){
     $window.alert("Running animation");
     $window.setInterval(function moveBees(){
         $(".bees").addClass("animate");
